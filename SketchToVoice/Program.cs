512b1263de89c7e00198d2d32165043448b7c276
Rami Hikmat <rami@ramihikmat.net>
2020-11-06 12:48:13 +1100

    Upload to bucket

---
+++ SketchToVoice/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace SketchToVoice
+{
+    class Program
+    {
+        public const string FileNameToLookFor = "sample.png";
+        
+        public const string S3BucketName = "project-sketch-to-voice";
+        public const string SketchDir = "sketch";
+        public const string TextDir = "text";
+        public const string AudioDir = "audio";
+
+        private static AmazonS3Client _s3Client;
+
+        static Program()
+        {
+            _s3Client = new AmazonS3Client();
+        }
+
+        static async Task Main(string[] args)
+        {
+            FileStream reader = GetFile();
+            await CreateS3BucketIfNotExists();
+            string sketchPath = await UploadToBucket(reader);
+            string text = await ReadText(sketchPath);
+            string voiceUrl = await GetVoice(text);
+        }
+
+        private static FileStream GetFile()
+        {
+            return new FileStream(FileNameToLookFor, FileMode.Open, FileAccess.Read);
+        }
+        
+        private static async Task CreateS3BucketIfNotExists()
+        {
+            var bucketsResponse = await _s3Client.ListBucketsAsync(new ListBucketsRequest());
+            
+            var bucket = new S3Bucket
+            {
+                BucketName = S3BucketName
+            };
+            if (!bucketsResponse.Buckets.Contains(bucket))
+            {
+                var request = new PutBucketRequest
+                {
+                    BucketName = S3BucketName,
+                    UseClientRegion = true
+                };
+
+                await _s3Client.PutBucketAsync(request);
+            }
+        }
+        
+        private static async Task<string> UploadToBucket(FileStream reader)
+        {
+            string path = $"{SketchDir}/{FileNameToLookFor}";
+            var request = new PutObjectRequest
+            {
+                BucketName = S3BucketName,
+                Key = path,
+                InputStream = reader,
+            };
+
+            await _s3Client.PutObjectAsync(request);
+            return path;
+        }
+        
+        private static async Task<string> ReadText(string textUrl)
+        {
+            throw new NotImplementedException();
+        }
+        
+        private static async Task<string> GetVoice(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
